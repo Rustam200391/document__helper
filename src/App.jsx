ad254407be7f7e608f02837e008a8e9ec86b7066
Rustam Guseinov <rg20031991.com>
2023-10-20 19:25:31 +0300

    build  pages&router commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Login } from "./pages/login/Login";
+import { QPage } from "./pages/questionPage/questionPage";
 import "../src/styles/App.css";
-
 
 function App() {
   return (
     <>
-      <Login />
+      <Router>
+        <Fragment>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/qpage" element={<QPage />} />
+          </Routes>
+        </Fragment>
+      </Router>
     </>
   );
 }
