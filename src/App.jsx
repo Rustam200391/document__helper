6ce6ddff43aade28f8da49ff8bcedb350de5f94e
Rustam Guseinov <rg20031991.com>
2023-10-21 13:41:58 +0300

    add pages route in app

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { Fragment } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Login } from "./pages/login/Login";
 import { QPage } from "./pages/questionPage/questionPage";
+import { MPage } from "./pages/mainPage/mainPage";
 import "../src/styles/App.css";
 
 function App() {
@@ -10,7 +11,9 @@ function App() {
       <Router>
         <Fragment>
           <Routes>
+            <Route path="/main" element={<MPage />} />
             <Route path="/" element={<Login />} />
+
             <Route path="/qpage" element={<QPage />} />
           </Routes>
         </Fragment>
